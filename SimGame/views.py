380dbb6bb3c48b253c335591e3af8f30344124e7
Esotericcat <olek.powroznik@gmail.com>
2023-07-28 13:24:23 +0200

    Addded Html and Css

--- SimGame/views.py
+++ SimGame/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
 
 # Create your views here.
