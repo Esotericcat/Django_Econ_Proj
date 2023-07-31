36a41cd724b74f6f39d7034e5d67d5a9e78e22e2
Esotericcat <olek.powroznik@gmail.com>
2023-07-31 11:43:47 +0200

    Model Changes

--- SimGame/models.py
+++ SimGame/models.py
@@ -1,25 +1,39 @@
 from django.db import models
 
-class Player(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=64)
-    money = models.IntegerField(default=500)
-    def __str__(self):
-        return self.name
-class Good(models.Model):
-    name = models.CharField(max_length=64)
-    price = models.IntegerField(default=0)
-    def __str__(self):
-        return self.name
 
+
+class UserBalance(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Goods(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Seller(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class SellerGoods(models.Model):
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
 
 class Transaction(models.Model):
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    good = models.ForeignKey(Good, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=0)
-    transaction_type = models.CharField(max_length=64)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        return f'{self.player.name} {self.good.name} {self.quantity}'
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    type = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
 
 
-# Create your models here.
+
+
+
+
